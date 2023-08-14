3dd7d1ab212579cf0e7734593040ae767399988b
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2023-08-14 21:14:40 +0900

    Update Presets (5)

--- Digital-Synth-PRA32-U/pra32-u-program-table.h
+++ Digital-Synth-PRA32-U/pra32-u-program-table.h
@@ -5,20 +5,20 @@ const uint8_t PROGRAM_NUMBER_DEFAULT = 0;
 // PRESET                                   #0   #1   #2   #3   #4   #5   #6   #7     #8   #9   #10  #11  #12  #13  #14  #15  
 uint8_t g_preset_table_OSC_1_WAVE     [] = {96 , 96 , 96 , 96 , 96 , 96 , 96 , 0  ,   96 , 96 , 96 , 96 , 96 , 96 , 96 , 0  };
 uint8_t g_preset_table_OSC_1_SHAPE    [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  ,   0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
-uint8_t g_preset_table_OSC_1_MORPH    [] = {64 , 127, 0  , 64 , 64 , 0  , 64 , 0  ,   64 , 127, 0  , 64 , 64 , 0  , 64 , 0  };
+uint8_t g_preset_table_OSC_1_MORPH    [] = {64 , 64 , 64 , 64 , 64 , 0  , 64 , 0  ,   64 , 64 , 64 , 64 , 64 , 0  , 64 , 0  };
 uint8_t g_preset_table_MIXER_SUB_OSC  [] = {127, 64 , 64 , 64 , 127, 64 , 64 , 64 ,   127, 64 , 64 , 64 , 127, 64 , 64 , 64 };
 
-uint8_t g_preset_table_OSC_2_WAVE     [] = {0  , 0  , 127, 0  , 0  , 0  , 0  , 0  ,   0  , 0  , 127, 0  , 0  , 0  , 0  , 0  };
+uint8_t g_preset_table_OSC_2_WAVE     [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  ,   0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
 uint8_t g_preset_table_OSC_2_COARSE   [] = {71 , 64 , 64 , 64 , 64 , 64 , 64 , 64 ,   71 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
-uint8_t g_preset_table_OSC_2_FINE     [] = {72 , 72 , 72 , 72 , 72 , 72 , 72 , 64 ,   72 , 72 , 72 , 72 , 72 , 72 , 72 , 64 };
-uint8_t g_preset_table_MIXER_OSC_MIX  [] = {64 , 64 , 64 , 64 , 64 , 64 , 64 , 0  ,   64 , 64 , 64 , 64 , 64 , 64 , 64 , 0  };
+uint8_t g_preset_table_OSC_2_FINE     [] = {70 , 70 , 70 , 70 , 70 , 70 , 70 , 64 ,   70 , 70 , 70 , 70 , 70 , 70 , 70 , 64 };
+uint8_t g_preset_table_MIXER_OSC_MIX  [] = {64 , 32 , 32 , 32 , 64 , 64 , 32 , 0  ,   64 , 32 , 32 , 32 , 64 , 64 , 32 , 0  };
 
-uint8_t g_preset_table_FILTER_CUTOFF  [] = {115, 115, 73 , 121, 61 , 115, 127, 127,   115, 115, 73 , 121, 61 , 115, 127, 127};
+uint8_t g_preset_table_FILTER_CUTOFF  [] = {115, 115, 67 , 109, 61 , 115, 127, 127,   115, 115, 67 , 109, 61 , 115, 127, 127};
 uint8_t g_preset_table_FILTER_RESO    [] = {64 , 32 , 32 , 32 , 64 , 64 , 0  , 0  ,   64 , 32 , 32 , 32 , 64 , 64 , 0  , 0  };
 uint8_t g_preset_table_FILTER_EG_AMT  [] = {64 , 64 , 112, 4  , 124, 64 , 64 , 64 ,   64 , 64 , 112, 4  , 124, 64 , 64 , 64 };
 uint8_t g_preset_table_FILTER_KEY_TRK [] = {127, 127, 127, 127, 127, 127, 127, 127,   127, 127, 127, 127, 127, 127, 127, 127};
 
-uint8_t g_preset_table_EG_ATTACK      [] = {0  , 0  , 64 , 96 , 0  , 0  , 0  , 0  ,   0  , 0  , 64 , 96 , 0  , 0  , 0  , 0  };
+uint8_t g_preset_table_EG_ATTACK      [] = {0  , 0  , 0  , 96 , 0  , 0  , 0  , 0  ,   0  , 0  , 0  , 96 , 0  , 0  , 0  , 0  };
 uint8_t g_preset_table_EG_DECAY       [] = {0  , 0  , 96 , 96 , 96 , 0  , 0  , 0  ,   0  , 0  , 96 , 96 , 96 , 0  , 0  , 0  };
 uint8_t g_preset_table_EG_SUSTAIN     [] = {127, 127, 0  , 0  , 0  , 127, 127, 127,   127, 127, 0  , 0  , 0  , 127, 127, 127};
 uint8_t g_preset_table_EG_RELEASE     [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  ,   0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
@@ -44,7 +44,7 @@ uint8_t g_preset_table_AMP_SUSTAIN    [] = {127, 127, 127, 127, 0  , 127, 127, 1
 uint8_t g_preset_table_AMP_RELEASE    [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  ,   0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
 
 uint8_t g_preset_table_CHORUS_MODE    [] = {127, 127, 127, 127, 127, 127, 127, 0  ,   127, 127, 127, 127, 127, 127, 127, 0  };
-uint8_t g_preset_table_CHORUS_RATE    [] = {32 , 32 , 32 , 32 , 32 , 32 , 32 , 32 ,   32 , 32 , 32 , 32 , 32 , 32 , 32 , 32 };
+uint8_t g_preset_table_CHORUS_RATE    [] = {48 , 48 , 48 , 48 , 48 , 48 , 48 , 48 ,   48 , 48 , 48 , 48 , 48 , 48 , 48 , 48 };
 uint8_t g_preset_table_CHORUS_DEPTH   [] = {32 , 32 , 32 , 32 , 32 , 32 , 32 , 32 ,   32 , 32 , 32 , 32 , 32 , 32 , 32 , 32 };
 uint8_t g_preset_table_CHORUS_DLY_TIME[] = {64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 ,   64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
 
