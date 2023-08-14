e4c3956c03b6e7e18257db399cce912256f7075f
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2023-08-14 10:52:37 +0900

    Update Presets (3)

--- Digital-Synth-PRA32-U/pra32-u-program-table.h
+++ Digital-Synth-PRA32-U/pra32-u-program-table.h
@@ -5,7 +5,7 @@ const uint8_t PROGRAM_NUMBER_DEFAULT = 0;
 // PRESET                                   #0   #1   #2   #3   #4   #5   #6   #7     #8   #9   #10  #11  #12  #13  #14  #15  
 uint8_t g_preset_table_OSC_1_WAVE     [] = {96 , 96 , 96 , 96 , 96 , 96 , 96 , 0  ,   96 , 96 , 96 , 96 , 96 , 96 , 96 , 0  };
 uint8_t g_preset_table_OSC_1_SHAPE    [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  ,   0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
-uint8_t g_preset_table_OSC_1_MORPH    [] = {64 , 64 , 0  , 64 , 64 , 0  , 66 , 0  ,   64 , 64 , 0  , 64 , 64 , 0  , 66 , 0  };
+uint8_t g_preset_table_OSC_1_MORPH    [] = {64 , 96 , 0  , 64 , 64 , 0  , 66 , 0  ,   64 , 96 , 0  , 64 , 64 , 0  , 66 , 0  };
 uint8_t g_preset_table_MIXER_SUB_OSC  [] = {127, 64 , 64 , 64 , 127, 64 , 64 , 64 ,   127, 64 , 64 , 64 , 127, 64 , 64 , 64 };
 
 uint8_t g_preset_table_OSC_2_WAVE     [] = {0  , 0  , 127, 0  , 0  , 0  , 0  , 0  ,   0  , 0  , 127, 0  , 0  , 0  , 0  , 0  };
