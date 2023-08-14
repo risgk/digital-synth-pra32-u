58cee50b7026613e6eac0f7541ba1f29eca93df7
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2023-08-14 22:36:50 +0900

    Update Presets (7)

--- Digital-Synth-PRA32-U/pra32-u-program-table.h
+++ Digital-Synth-PRA32-U/pra32-u-program-table.h
@@ -11,9 +11,9 @@ uint8_t g_preset_table_MIXER_SUB_OSC  [] = {127, 64 , 64 , 64 , 127, 64 , 64 , 6
 uint8_t g_preset_table_OSC_2_WAVE     [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  ,   0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
 uint8_t g_preset_table_OSC_2_COARSE   [] = {71 , 64 , 64 , 64 , 64 , 64 , 64 , 64 ,   71 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
 uint8_t g_preset_table_OSC_2_FINE     [] = {70 , 70 , 70 , 70 , 70 , 70 , 70 , 64 ,   70 , 70 , 70 , 70 , 70 , 70 , 70 , 64 };
-uint8_t g_preset_table_MIXER_OSC_MIX  [] = {64 , 32 , 32 , 32 , 64 , 64 , 32 , 0  ,   64 , 32 , 32 , 32 , 64 , 64 , 32 , 0  };
+uint8_t g_preset_table_MIXER_OSC_MIX  [] = {64 , 64 , 64 , 64 , 64 , 64 , 32 , 0  ,   64 , 64 , 64 , 64 , 64 , 64 , 32 , 0  };
 
-uint8_t g_preset_table_FILTER_CUTOFF  [] = {115, 115, 67 , 115, 49 , 115, 127, 127,   115, 115, 67 , 115, 49 , 115, 127, 127};
+uint8_t g_preset_table_FILTER_CUTOFF  [] = {115, 115, 67 , 115, 67 , 115, 127, 127,   115, 115, 67 , 115, 67 , 115, 127, 127};
 uint8_t g_preset_table_FILTER_RESO    [] = {64 , 32 , 32 , 32 , 64 , 64 , 0  , 0  ,   64 , 32 , 32 , 32 , 64 , 64 , 0  , 0  };
 uint8_t g_preset_table_FILTER_EG_AMT  [] = {64 , 64 , 112, 4  , 112, 64 , 64 , 64 ,   64 , 64 , 112, 4  , 112, 64 , 64 , 64 };
 uint8_t g_preset_table_FILTER_KEY_TRK [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  ,   0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
@@ -30,8 +30,8 @@ uint8_t g_preset_table_PORTAMENTO     [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0
 
 uint8_t g_preset_table_LFO_WAVE       [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  ,   0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
 uint8_t g_preset_table_LFO_RATE       [] = {96 , 96 , 96 , 96 , 96 , 80 , 96 , 96 ,   96 , 96 , 96 , 96 , 96 , 80 , 96 , 96 };
-uint8_t g_preset_table_LFO_DEPTH      [] = {8  , 64 , 64 , 64 , 0  , 127, 64 , 0  ,   8  , 64 , 64 , 64 , 0  , 127, 64 , 0  };
-uint8_t g_preset_table_LFO_FADE_TIME  [] = {0  , 64 , 64 , 64 , 0  , 0  , 64 , 0  ,   0  , 64 , 64 , 64 , 0  , 0  , 64 , 0  };
+uint8_t g_preset_table_LFO_DEPTH      [] = {0  , 0  , 0  , 0  , 0  , 127, 0  , 0  ,   0  , 0  , 0  , 0  , 0  , 127, 0  , 0  };
+uint8_t g_preset_table_LFO_FADE_TIME  [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  ,   0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
 
 uint8_t g_preset_table_LFO_OSC_AMT    [] = {80 , 80 , 80 , 80 , 80 , 96 , 80 , 64 ,   80 , 80 , 80 , 80 , 80 , 96 , 80 , 64 };
 uint8_t g_preset_table_LFO_OSC_DST    [] = {0  , 0  , 0  , 0  , 0  , 127, 0  , 0  ,   0  , 0  , 0  , 0  , 0  , 127, 0  , 0  };
