de8b10daee22873cddb3065a7871a0cc7847c510
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2023-08-17 14:59:17 +0900

    Update pra32-u-generate-filter-table.rb

--- Digital-Synth-PRA32-U/pra32-u-generate-filter-table.rb
+++ Digital-Synth-PRA32-U/pra32-u-generate-filter-table.rb
@@ -1,7 +1,5 @@
 require_relative 'pra32-u-constants'
 
-FILTER_A4_FREQ = 440.0
-
 $file = File.open("pra32-u-filter-table.h", "w")
 
 $file.printf("#pragma once\n\n")
@@ -12,7 +10,7 @@ def generate_filter_lpf_table(res_idx, name, q)
   $file.printf("int32_t g_filter_lpf_table_%s[] = {\n  ", name)
   (0..DATA_BYTE_MAX * 2 + 1).each do |i|
     f_idx = [[-2, i - 1 * 2].max, 252].min
-    f_0 = (2.0 ** ((f_idx / 2.0) / (120.0 / OCTAVES))) * ((FILTER_A4_FREQ * 2.0) * 16.0) * 2.0 / (2.0 ** (OCTAVES.to_f + 1.0))
+    f_0 = (2.0 ** ((f_idx / 2.0) / (120.0 / OCTAVES))) * ((A4_FREQ * 2.0) * 16.0) * 2.0 / (2.0 ** (OCTAVES.to_f + 1.0))
     f_0_over_f_s = f_0 / SAMPLING_RATE
 
     w_0 = 2.0 * Math::PI * f_0_over_f_s
