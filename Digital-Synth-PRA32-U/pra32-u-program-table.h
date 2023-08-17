362754db250b90db98824b4e558375cbc4dab786
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2023-08-17 15:48:44 +0900

    Update Presets

--- Digital-Synth-PRA32-U/pra32-u-program-table.h
+++ Digital-Synth-PRA32-U/pra32-u-program-table.h
@@ -18,10 +18,10 @@ uint8_t g_preset_table_FILTER_RESO    [] = {64 , 64 , 32 , 32 , 64 , 64 , 32 , 0
 uint8_t g_preset_table_FILTER_EG_AMT  [] = {64 , 64 , 100, 16 , 112, 64 , 76 , 64 ,   64 , 64 , 100, 16 , 112, 64 , 76 , 64 };
 uint8_t g_preset_table_FILTER_KEY_TRK [] = {127, 127, 127, 127, 127, 127, 127, 127,   127, 127, 127, 127, 127, 127, 127, 127};
 
-uint8_t g_preset_table_EG_ATTACK      [] = {0  , 32 , 16 , 96 , 0  , 0  , 0  , 0  ,   0  , 32 , 16 , 96 , 0  , 0  , 0  , 0  };
-uint8_t g_preset_table_EG_DECAY       [] = {0  , 0  , 64 , 96 , 96 , 0  , 112, 0  ,   0  , 0  , 64 , 96 , 96 , 0  , 112, 0  };
+uint8_t g_preset_table_EG_ATTACK      [] = {0  , 64 , 48 , 96 , 0  , 0  , 0  , 0  ,   0  , 64 , 48 , 96 , 0  , 0  , 0  , 0  };
+uint8_t g_preset_table_EG_DECAY       [] = {0  , 0  , 80 , 96 , 96 , 0  , 104, 0  ,   0  , 0  , 80 , 96 , 96 , 0  , 104, 0  };
 uint8_t g_preset_table_EG_SUSTAIN     [] = {127, 127, 0  , 0  , 0  , 127, 0  , 127,   127, 127, 0  , 0  , 0  , 127, 0  , 127};
-uint8_t g_preset_table_EG_RELEASE     [] = {0  , 32 , 0  , 0  , 96 , 0  , 0  , 0  ,   0  , 32 , 0  , 0  , 96 , 0  , 0  , 0  };
+uint8_t g_preset_table_EG_RELEASE     [] = {0  , 64 , 0  , 0  , 96 , 0  , 0  , 0  ,   0  , 64 , 0  , 0  , 96 , 0  , 0  , 0  };
 
 uint8_t g_preset_table_EG_OSC_AMT     [] = {64 , 64 , 70 , 64 , 64 , 64 , 96 , 64 ,   64 , 64 , 70 , 64 , 64 , 64 , 96 , 64 };
 uint8_t g_preset_table_EG_OSC_DST     [] = {0  , 0  , 64 , 0  , 0  , 0  , 127, 0  ,   0  , 0  , 64 , 0  , 0  , 0  , 127, 0  };
@@ -38,10 +38,10 @@ uint8_t g_preset_table_LFO_OSC_DST    [] = {0  , 0  , 0  , 0  , 0  , 127, 0  , 0
 uint8_t g_preset_table_LFO_FILTER_AMT [] = {64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 ,   64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
 uint8_t g_preset_table_AMP_LEVEL      [] = {64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 ,   64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
 
-uint8_t g_preset_table_AMP_ATTACK     [] = {0  , 32 , 0  , 0  , 0  , 0  , 0  , 0  ,   0  , 32 , 0  , 0  , 0  , 0  , 0  , 0  };
-uint8_t g_preset_table_AMP_DECAY      [] = {0  , 0  , 0  , 0  , 96 , 0  , 112, 0  ,   0  , 0  , 0  , 0  , 96 , 0  , 112, 0  };
+uint8_t g_preset_table_AMP_ATTACK     [] = {0  , 64 , 0  , 0  , 0  , 0  , 0  , 0  ,   0  , 64 , 0  , 0  , 0  , 0  , 0  , 0  };
+uint8_t g_preset_table_AMP_DECAY      [] = {0  , 0  , 0  , 0  , 96 , 0  , 104, 0  ,   0  , 0  , 0  , 0  , 96 , 0  , 104, 0  };
 uint8_t g_preset_table_AMP_SUSTAIN    [] = {127, 127, 127, 127, 0  , 127, 0  , 127,   127, 127, 127, 127, 0  , 127, 0  , 127};
-uint8_t g_preset_table_AMP_RELEASE    [] = {0  , 32 , 0  , 0  , 96 , 0  , 0  , 0  ,   0  , 32 , 0  , 0  , 96 , 0  , 0  , 0  };
+uint8_t g_preset_table_AMP_RELEASE    [] = {0  , 64 , 0  , 0  , 96 , 0  , 0  , 0  ,   0  , 64 , 0  , 0  , 96 , 0  , 0  , 0  };
 
 uint8_t g_preset_table_CHORUS_MODE    [] = {127, 127, 127, 127, 127, 127, 127, 0  ,   127, 127, 127, 127, 127, 127, 127, 0  };
 uint8_t g_preset_table_CHORUS_RATE    [] = {32 , 32 , 32 , 32 , 32 , 32 , 32 , 32 ,   32 , 32 , 32 , 32 , 32 , 32 , 32 , 32 };
