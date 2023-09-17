ebd6d8ebd9dd0bb0593e483b315c830465f2ab8a
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2023-09-17 19:31:38 +0900

    Change Amp Level to Amp Gain (3)

--- Digital-Synth-PRA32-U/pra32-u-amp.h
+++ Digital-Synth-PRA32-U/pra32-u-amp.h
@@ -3,21 +3,20 @@
 #include "pra32-u-common.h"
 
 class PRA32_U_Amp {
-  int16_t m_gain;
-  int16_t m_gain_effective;
+  int16_t m_gain_control;
+  int16_t m_gain_control_effective;
   int16_t m_gain_mod_input;
 
 public:
 PRA32_U_Amp()
-  : m_gain()
-  , m_gain_effective()
-  , m_gain_mod_input()
+  : m_gain_control(127)
+  , m_gain_control_effective(0)
+  , m_gain_mod_input(0)
   {
-    set_gain(127);
   }
 
   INLINE void set_gain(uint8_t controller_value) {
-    m_gain = ((controller_value * controller_value) * 16384) / 16129;
+    m_gain_control = controller_value;
   }
 
   INLINE void process_at_low_rate(int16_t gain_mod_input) {
@@ -27,16 +26,14 @@ PRA32_U_Amp()
 
   INLINE int16_t process(int16_t audio_input) {
     int16_t audio_output = (audio_input * m_gain_mod_input) >> 14;
-    audio_output = (audio_output * m_gain_effective) >> 14;
+    int16_t gain_linear = ((m_gain_control_effective * m_gain_control_effective) * 16384) / 16129;
+    audio_output = (audio_output * gain_linear) >> 14;
     return audio_output;
   }
 
 private:
   INLINE void update_gain_effective() {
-    // effective_new = clamp(m_gain, (m_gain_effective - 256), (m_gain_effective + 256))
-    volatile int32_t effective_new = m_gain - (m_gain_effective + 256);
-    effective_new = (effective_new < 0) * effective_new + (m_gain_effective + 256) - (m_gain_effective - 256);
-    effective_new = (effective_new > 0) * effective_new + (m_gain_effective - 256);
-    m_gain_effective = effective_new;
+    m_gain_control_effective += (m_gain_control_effective < m_gain_control);
+    m_gain_control_effective -= (m_gain_control_effective > m_gain_control);
   }
 };
