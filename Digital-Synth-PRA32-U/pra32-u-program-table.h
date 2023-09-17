9a71d9ccf76fb0014a42ef2df0d88f36b79c4a80
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2023-09-17 10:32:50 +0900

    Update Presets (2)

--- Digital-Synth-PRA32-U/pra32-u-program-table.h
+++ Digital-Synth-PRA32-U/pra32-u-program-table.h
@@ -4,8 +4,8 @@ const uint8_t PROGRAM_NUMBER_DEFAULT = 0;
 
 // PRESET                                   #0   #1   #2   #3   #4   #5   #6   #7     #8   #9   #10  #11  #12  #13  #14  #15  
 uint8_t g_preset_table_OSC_1_WAVE     [] = {100, 100, 100, 100, 100, 100, 100, 0  ,   100, 100, 100, 100, 100, 100, 100, 0  };
-uint8_t g_preset_table_OSC_1_SHAPE    [] = {0  , 0  , 0  , 0  , 0  , 0  , 127, 0  ,   0  , 0  , 0  , 0  , 0  , 0  , 127, 0  };
-uint8_t g_preset_table_OSC_1_MORPH    [] = {64 , 64 , 64 , 64 , 64 , 0  , 127, 0  ,   64 , 64 , 64 , 64 , 64 , 0  , 127, 0  };
+uint8_t g_preset_table_OSC_1_SHAPE    [] = {0  , 0  , 0  , 0  , 0  , 0  , 96 , 0  ,   0  , 0  , 0  , 0  , 0  , 0  , 96 , 0  };
+uint8_t g_preset_table_OSC_1_MORPH    [] = {64 , 64 , 64 , 64 , 64 , 0  , 0  , 0  ,   64 , 64 , 64 , 64 , 64 , 0  , 0  , 0  };
 uint8_t g_preset_table_MIXER_SUB_OSC  [] = {127, 64 , 64 , 64 , 127, 64 , 64 , 64 ,   127, 64 , 64 , 64 , 127, 64 , 64 , 64 };
 
 uint8_t g_preset_table_OSC_2_WAVE     [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  ,   0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
@@ -23,7 +23,7 @@ uint8_t g_preset_table_EG_DECAY       [] = {0  , 0  , 80 , 96 , 96 , 0  , 104, 0
 uint8_t g_preset_table_EG_SUSTAIN     [] = {127, 127, 0  , 0  , 0  , 127, 0  , 127,   127, 127, 0  , 0  , 0  , 127, 0  , 127};
 uint8_t g_preset_table_EG_RELEASE     [] = {0  , 64 , 0  , 0  , 96 , 0  , 0  , 0  ,   0  , 64 , 0  , 0  , 96 , 0  , 0  , 0  };
 
-uint8_t g_preset_table_EG_OSC_AMT     [] = {64 , 64 , 70 , 64 , 64 , 64 , 32 , 64 ,   64 , 64 , 70 , 64 , 64 , 64 , 32 , 64 };
+uint8_t g_preset_table_EG_OSC_AMT     [] = {64 , 64 , 70 , 64 , 64 , 64 , 40 , 64 ,   64 , 64 , 70 , 64 , 64 , 64 , 40 , 64 };
 uint8_t g_preset_table_EG_OSC_DST     [] = {0  , 0  , 64 , 0  , 0  , 0  , 127, 0  ,   0  , 0  , 64 , 0  , 0  , 0  , 127, 0  };
 uint8_t g_preset_table_VOICE_MODE     [] = {127, 0  , 0  , 0  , 75 , 127, 0  , 0  ,   127, 0  , 0  , 0  , 75 , 127, 0  , 0  };
 uint8_t g_preset_table_PORTAMENTO     [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  ,   0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
