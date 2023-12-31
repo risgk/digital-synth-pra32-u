a1bb47ec5c62f200e73a1e956896b40b0ad104a6
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2023-12-31 18:28:03 +0900

    Revert "Revert "Add PWM audio output option (1)""
    This reverts commit c9906e3b2e00df01c589191fd25a9c906654e1e6.

--- Digital-Synth-PRA32-U/Digital-Synth-PRA32-U.ino
+++ Digital-Synth-PRA32-U/Digital-Synth-PRA32-U.ino
@@ -4,13 +4,16 @@
 
 //#define DEBUG_PRINT
 
+
 #define USE_USB_MIDI      // Select USB Stack: "Adafruit TinyUSB" in the Arduino IDE "Tools" menu
 //#define USE_SERIAL1_MIDI
 
 #define SERIAL1_MIDI_SPEED              (31250)
 //#define SERIAL1_MIDI_SPEED              (38400)
 
+
 #define MIDI_CH                         (0)  // 0-based
+
 
 // for Pimoroni Pico Audio Pack [PIM544]
 #define I2S_DAC_MUTE_OFF_PIN            (22)
@@ -23,12 +26,22 @@
 #define I2S_BUFFERS                     (4)
 #define I2S_BUFFER_WORDS                (64)
 
+
+//#define USE_PWM_AUDIO
+
+#define PWM_AUDIO_L_PIN                 (28)
+#define PWM_AUDIO_R_PIN                 (27)
+
+#define PWM_AUDIO_BUFFERS               (4)
+#define PWM_AUDIO_BUFFER_WORDS          (64)
+
 ////////////////////////////////////////////////////////////////
 
 #include "pra32-u-common.h"
 #include "pra32-u-synth.h"
 
 PRA32_U_Synth g_synth;
+
 
 #include <MIDI.h>
 #if defined(USE_USB_MIDI)
@@ -37,11 +50,18 @@ Adafruit_USBD_MIDI usbd_midi;
 MIDI_CREATE_INSTANCE(Adafruit_USBD_MIDI, usbd_midi, MIDI);
 #elif defined(USE_SERIAL1_MIDI)
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
-#endif
+#endif // defined(USE_USB_MIDI)
 
+
+#if defined(USE_PWM_AUDIO)
+#include <PWMAudio.h>
+PWMAudio g_pwm_l(PWM_AUDIO_L_PIN);
+PWMAudio g_pwm_r(PWM_AUDIO_R_PIN);
+#else // defined(USE_PWM_AUDIO)
 #include <I2S.h>
-
 I2S g_i2s_output(OUTPUT);
+#endif // defined(USE_PWM_AUDIO)
+
 
 void handleNoteOn(byte channel, byte pitch, byte velocity);
 void handleNoteOff(byte channel, byte pitch, byte velocity);
@@ -49,20 +69,30 @@ void handleControlChange(byte channel, byte number, byte value);
 void handleHandleProgramChange(byte channel, byte number);
 void handleHandlePitchBend(byte channel, int bend);
 
+
 void __not_in_flash_func(setup)() {
 }
 
 void __not_in_flash_func(loop)() {
 }
 
+
 void __not_in_flash_func(setup1)() {
+#if defined(USE_PWM_AUDIO)
+  g_pwm_l.setBuffers(PWM_AUDIO_BUFFERS, PWM_AUDIO_BUFFER_WORDS / 2);
+  g_pwm_r.setBuffers(PWM_AUDIO_BUFFERS, PWM_AUDIO_BUFFER_WORDS / 2);
+  g_pwm_l.setFrequency(SAMPLING_RATE);
+  g_pwm_r.setFrequency(SAMPLING_RATE);
+  g_pwm_l.begin();
+  g_pwm_r.begin();
+#else // defined(USE_PWM_AUDIO)
   g_i2s_output.setSysClk(SAMPLING_RATE);
   g_i2s_output.setFrequency(SAMPLING_RATE);
   g_i2s_output.setDATA(I2S_DATA_PIN);
 #if defined(I2S_MCLK_PIN)
   g_i2s_output.setMCLK(I2S_MCLK_PIN);
   g_i2s_output.setMCLKmult(I2S_MCLK_MULT);
-#endif
+#endif // defined(I2S_MCLK_PIN)
   g_i2s_output.setBCLK(I2S_BCLK_PIN);
   if (I2S_SWAP_BCLK_AND_LRCLK_PINS) {
     g_i2s_output.swapClocks();
@@ -70,12 +100,14 @@ void __not_in_flash_func(setup1)() {
   g_i2s_output.setBitsPerSample(16);
   g_i2s_output.setBuffers(I2S_BUFFERS, I2S_BUFFER_WORDS);
   g_i2s_output.begin();
+#endif // defined(USE_PWM_AUDIO)
+
 
 #if defined(USE_USB_MIDI)
   TinyUSB_Device_Init(0);
   USBDevice.setManufacturerDescriptor("ISGK Instruments");
   USBDevice.setProductDescriptor("Digital Synth PRA32-U");
-#endif
+#endif // defined(USE_USB_MIDI)
   MIDI.setHandleNoteOn(handleNoteOn);
   MIDI.setHandleNoteOff(handleNoteOff);
   MIDI.setHandleControlChange(handleControlChange);
@@ -85,30 +117,64 @@ void __not_in_flash_func(setup1)() {
   MIDI.turnThruOff();
 #if defined(USE_SERIAL1_MIDI)
   Serial1.begin(SERIAL1_MIDI_SPEED);
-#endif
+#endif // defined(USE_SERIAL1_MIDI)
+
 
 #if defined(DEBUG_PRINT)
 #if defined(USE_SERIAL1_MIDI)
   Serial.begin(0);  // Select USB Stack: "Pico SDK" in the Arduino IDE "Tools" menu
-#else
+#else // defined(USE_SERIAL1_MIDI)
   Serial1.begin(115200);
-#endif
-#endif
+#endif // defined(USE_SERIAL1_MIDI)
+#endif // defined(DEBUG_PRINT)
+
 
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
 
+
+#if defined(USE_PWM_AUDIO)
+#else // defined(USE_PWM_AUDIO)
 #if defined(I2S_DAC_MUTE_OFF_PIN)
   pinMode(I2S_DAC_MUTE_OFF_PIN, OUTPUT);
   digitalWrite(I2S_DAC_MUTE_OFF_PIN, HIGH);
-#endif
+#endif // defined(I2S_DAC_MUTE_OFF_PIN)
+#endif // defined(USE_PWM_AUDIO)
 }
 
 void __not_in_flash_func(loop1)() {
 
 #if defined(DEBUG_PRINT)
   uint32_t debug_measurement_start0_us = micros();
-#endif
+#endif // defined(DEBUG_PRINT)
+
+
+#if defined(USE_PWM_AUDIO)
+
+  for (uint32_t i = 0; i < (PWM_AUDIO_BUFFER_WORDS + 15) / 16; i++) {
+    MIDI.read();
+  }
+
+#if defined(DEBUG_PRINT)
+  uint32_t debug_measurement_start1_us = micros();
+#endif // defined(DEBUG_PRINT)
+
+  int16_t left_buffer[PWM_AUDIO_BUFFER_WORDS];
+  int16_t right_buffer[PWM_AUDIO_BUFFER_WORDS];
+  for (uint32_t i = 0; i < PWM_AUDIO_BUFFER_WORDS; i++) {
+    left_buffer[i] = g_synth.process(right_buffer[i]);
+  }
+
+#if defined(DEBUG_PRINT)
+  uint32_t debug_measurement_end_us = micros();
+#endif // defined(DEBUG_PRINT)
+
+  for (uint32_t i = 0; i < I2S_BUFFER_WORDS; i++) {
+    g_pwm_l.write(left_buffer[i]);
+    g_pwm_r.write(right_buffer[i]);
+  }
+
+#else // defined(USE_PWM_AUDIO)
 
   for (uint32_t i = 0; i < (I2S_BUFFER_WORDS + 15) / 16; i++) {
     MIDI.read();
@@ -116,7 +182,7 @@ void __not_in_flash_func(loop1)() {
 
 #if defined(DEBUG_PRINT)
   uint32_t debug_measurement_start1_us = micros();
-#endif
+#endif // defined(DEBUG_PRINT)
 
   int16_t left_buffer[I2S_BUFFER_WORDS];
   int16_t right_buffer[I2S_BUFFER_WORDS];
@@ -126,11 +192,14 @@ void __not_in_flash_func(loop1)() {
 
 #if defined(DEBUG_PRINT)
   uint32_t debug_measurement_end_us = micros();
-#endif
+#endif // defined(DEBUG_PRINT)
 
   for (uint32_t i = 0; i < I2S_BUFFER_WORDS; i++) {
     g_i2s_output.write16(left_buffer[i], right_buffer[i]);
   }
+
+#endif // defined(USE_PWM_AUDIO)
+
 
 #if defined(DEBUG_PRINT)
   static uint32_t s_debug_measurement_max0_us = 0;
@@ -152,15 +221,15 @@ void __not_in_flash_func(loop1)() {
     Serial.println(debug_measurement_elapsed0_us);
     Serial.println(s_debug_measurement_max0_us);
     Serial.println();
-#else
+#else // defined(USE_SERIAL1_MIDI)
     Serial1.println(debug_measurement_elapsed1_us);
     Serial1.println(s_debug_measurement_max1_us);
     Serial1.println(debug_measurement_elapsed0_us);
     Serial1.println(s_debug_measurement_max0_us);
     Serial1.println();
-#endif
+#endif // defined(USE_SERIAL1_MIDI)
   }
-#endif
+#endif // defined(DEBUG_PRINT)
 }
 
 void __not_in_flash_func(handleNoteOn)(byte channel, byte pitch, byte velocity)
