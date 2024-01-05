c358d9d79884e420626e2a136a5a1a0e511f5b05
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2024-01-05 20:23:43 +0900

    Added the function of writing user programs to the flash (7)

--- Digital-Synth-PRA32-U/Digital-Synth-PRA32-U.ino
+++ Digital-Synth-PRA32-U/Digital-Synth-PRA32-U.ino
@@ -140,6 +140,8 @@ void __not_in_flash_func(setup1)() {
   digitalWrite(I2S_DAC_MUTE_OFF_PIN, HIGH);
 #endif // defined(I2S_DAC_MUTE_OFF_PIN)
 #endif // defined(USE_PWM_AUDIO_INSTEAD_OF_I2S)
+
+  g_synth.initialize();
 }
 
 void __not_in_flash_func(loop1)() {
