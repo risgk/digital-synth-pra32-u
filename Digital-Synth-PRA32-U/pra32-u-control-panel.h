81aa3532d9426a9466a3bdba4ff5aef5a54656c5
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2024-03-20 09:34:44 +0900

    Implement panel functions (10)

--- Digital-Synth-PRA32-U/pra32-u-control-panel.h
+++ Digital-Synth-PRA32-U/pra32-u-control-panel.h
@@ -6,6 +6,7 @@
 
 static volatile uint32_t s_adc_current_value[3];
 static volatile uint32_t s_adc_control_value[3];
+static volatile uint8_t  s_adc_control_target[3] = {FILTER_CUTOFF  , FILTER_RESO    , FILTER_EG_AMT  };
 
 static char s_display_buffer[8][21 + 1] = {
   "                    5",
@@ -80,34 +81,37 @@ INLINE void PRA32_U_ControlPanel_update_input(uint32_t loop_counter) {
 #endif  // defined(PRA32_U_USE_CONTROL_PANEL)
 }
 
+static INLINE uint8_t PRA32_U_ControlPanel_adc_control_value_candidate(int adc_number) {
+#if defined(PRA32_U_CONTROL_PANEL_REVERSE_ANALOG_INPUT)
+  return (127 - (s_adc_current_value[adc_number] >> 2));
+#else  // defined(PRA32_U_CONTROL_PANEL_REVERSE_ANALOG_INPUT)
+  return (s_adc_current_value[adc_number] >> 2);
+#endif  // defined(PRA32_U_CONTROL_PANEL_REVERSE_ANALOG_INPUT)
+}
+
+static INLINE void PRA32_U_ControlPanel_update_adc_control(int adc_number) {
+  uint32_t adc_control_value_candidate = PRA32_U_ControlPanel_adc_control_value_candidate(adc_number);
+  if (s_adc_control_value[adc_number] != adc_control_value_candidate) {
+    s_adc_control_value[adc_number] = adc_control_value_candidate;
+    g_synth.control_change(s_adc_control_target[adc_number], s_adc_control_value[adc_number]);
+  }
+}
+
 INLINE void PRA32_U_ControlPanel_update_control() {
 #if defined(PRA32_U_USE_CONTROL_PANEL)
 #if defined(PRA32_U_USE_CONTROL_PANEL_ANALOG_INPUT)
   static uint32_t s_initialize_counter = 0;
-  if (s_initialize_counter < 16) {
+  if (s_initialize_counter < 2400) {
     ++s_initialize_counter;
-#if defined(PRA32_U_CONTROL_PANEL_REVERSE_ANALOG_INPUT)
-    s_adc_control_value[0] = 127 - (s_adc_current_value[0] >> 2);
-    s_adc_control_value[1] = 127 - (s_adc_current_value[1] >> 2);
-    s_adc_control_value[2] = 127 - (s_adc_current_value[2] >> 2);
-#else  // defined(PRA32_U_CONTROL_PANEL_REVERSE_ANALOG_INPUT)
-    s_adc_control_value[0] = s_adc_current_value[0] >> 2;
-    s_adc_control_value[1] = s_adc_current_value[1] >> 2;
-    s_adc_control_value[2] = s_adc_current_value[2] >> 2;
-#endif  // defined(PRA32_U_CONTROL_PANEL_REVERSE_ANALOG_INPUT)
+    s_adc_control_value[0] = PRA32_U_ControlPanel_adc_control_value_candidate(0);
+    s_adc_control_value[1] = PRA32_U_ControlPanel_adc_control_value_candidate(1);
+    s_adc_control_value[2] = PRA32_U_ControlPanel_adc_control_value_candidate(2);
     return;
   }
 
-#if defined(PRA32_U_CONTROL_PANEL_REVERSE_ANALOG_INPUT)
-  uint32_t adc_control_value_candidate = 127 - (s_adc_current_value[0] >> 2);
-#else  // defined(PRA32_U_CONTROL_PANEL_REVERSE_ANALOG_INPUT)
-  uint32_t adc_control_value_candidate = s_adc_current_value[0] >> 2;
-#endif  // defined(PRA32_U_CONTROL_PANEL_REVERSE_ANALOG_INPUT)
-
-  if (s_adc_control_value[0] != adc_control_value_candidate) {
-    s_adc_control_value[0] = adc_control_value_candidate;
-    g_synth.control_change(FILTER_CUTOFF  , s_adc_control_value[0]);
-  }
+  PRA32_U_ControlPanel_update_adc_control(0);
+  PRA32_U_ControlPanel_update_adc_control(1);
+  PRA32_U_ControlPanel_update_adc_control(2);
 #endif  // defined(PRA32_U_USE_CONTROL_PANEL_ANALOG_INPUT)
 #endif  // defined(PRA32_U_USE_CONTROL_PANEL)
 }
@@ -115,27 +119,36 @@ INLINE void PRA32_U_ControlPanel_update_control() {
 INLINE void PRA32_U_ControlPanel_update_display_buffer(uint32_t loop_counter) {
 #if defined(PRA32_U_USE_CONTROL_PANEL)
   if (loop_counter == 0) {
-    char buff[4];
+    char buff[6];
 
-    std::sprintf(buff, "%3u", g_synth.current_controller_value(FILTER_CUTOFF  ));
-    s_display_buffer[3][ 2] = buff[0];
-    s_display_buffer[3][ 3] = buff[1];
-    s_display_buffer[3][ 4] = buff[2];
+    uint8_t adc_control_target_0 = s_adc_control_target[0];
+    if (adc_control_target_0 <= 127) {
+      std::sprintf(buff, "%3u", g_synth.current_controller_value(adc_control_target_0));
+      s_display_buffer[3][ 2] = buff[0];
+      s_display_buffer[3][ 3] = buff[1];
+      s_display_buffer[3][ 4] = buff[2];
+    }
 
-    std::sprintf(buff, "%3u", g_synth.current_controller_value(FILTER_RESO    ));
-    s_display_buffer[3][13] = buff[0];
-    s_display_buffer[3][14] = buff[1];
-    s_display_buffer[3][15] = buff[2];
+    uint8_t adc_control_target_1 = s_adc_control_target[1];
+    if (adc_control_target_1 <= 127) {
+      std::sprintf(buff, "%3u", g_synth.current_controller_value(adc_control_target_1));
+      s_display_buffer[3][13] = buff[0];
+      s_display_buffer[3][14] = buff[1];
+      s_display_buffer[3][15] = buff[2];
+    }
 
-    std::sprintf(buff, "%3u", g_synth.current_controller_value(FILTER_EG_AMT  ));
-    s_display_buffer[7][13] = buff[0];
-    s_display_buffer[7][14] = buff[1];
-    s_display_buffer[7][15] = buff[2];
+    uint8_t adc_control_target_2 = s_adc_control_target[2];
+    if (adc_control_target_2 <= 127) {
+      std::sprintf(buff, "%3u", g_synth.current_controller_value(adc_control_target_2));
+      s_display_buffer[7][13] = buff[0];
+      s_display_buffer[7][14] = buff[1];
+      s_display_buffer[7][15] = buff[2];
 
-    std::sprintf(buff, "%+3d", static_cast<int32_t>(g_synth.current_controller_value(FILTER_EG_AMT  )) - 64);
-    s_display_buffer[7][17] = buff[0];
-    s_display_buffer[7][18] = buff[1];
-    s_display_buffer[7][19] = buff[2];
+      std::sprintf(buff, "%+3d", static_cast<int>(g_synth.current_controller_value(adc_control_target_2)) - 64);
+      s_display_buffer[7][17] = buff[0];
+      s_display_buffer[7][18] = buff[1];
+      s_display_buffer[7][19] = buff[2];
+    }
   }
 #endif  // defined(PRA32_U_USE_CONTROL_PANEL)
 }
